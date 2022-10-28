0f81ae2772aff6b40e845094b43178132f9a12b7
LosKeeper <thomas.dumond2001@gmail.com>
2022-10-28 19:21:02 +0200

    first commit

---
+++ html_parse.py
@@ -0,0 +1,37 @@
+import pycurl
+import certifi
+from io import BytesIO
+from datetime import datetime
+
+from config import URL
+
+def get_html() :
+
+    # Create a buffer to store the response
+    buffer = BytesIO()
+
+    # Create a pycurl object
+    c = pycurl.Curl()
+
+    # Set URL value
+    c.setopt(c.URL, URL)
+
+    # Write bytes that are utf-8 encoded
+    c.setopt(c.WRITEDATA, buffer)
+
+    # Set the User-Agent
+    c.setopt(c.USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36')
+
+
+    c.setopt(c.CAINFO, certifi.where())
+
+    # Perform a file transfer
+    c.perform()
+
+    # End curl session
+    c.close()
+
+    # Decode the buffer
+    body = buffer.getvalue().decode('utf-8')
+
+    print(body)
